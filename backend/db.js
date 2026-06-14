01b6c38dbe53cec986f0480d221555d7c904a325
Purple <dio30435@gmail.com>
2026-06-14 20:03:16 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -3,9 +3,8 @@ import pkg from "pg"
 const { Pool } = pkg
 
 export const pool = new Pool({
-    user: "todo_user",
-    host: "localhost",
-    database: "todo_app",
-    password: "123456",
-    port: 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
