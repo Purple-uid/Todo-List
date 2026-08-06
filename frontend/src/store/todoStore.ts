9916655988bc1268f15d522c6203f74785cb9e36
Purple <dio30435@gmail.com>
2026-08-06 21:19:51 +0300

    Update todoStore.ts

--- frontend/src/store/todoStore.ts
+++ frontend/src/store/todoStore.ts
@@ -12,7 +12,7 @@ interface TodoStore {
   deleteMessage: (id: number) => Promise<void>;
 }
 
-const API = "https://todo-list-03kl.onrender.com/api/todos/"
+const API = "https://todo-list-03kl.onrender.com/api/todos"
 
 export const useTodoStore = create<TodoStore>(
     (set) => ({
